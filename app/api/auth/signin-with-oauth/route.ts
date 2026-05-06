c69d5f97f735df592eeb8a9df635c7f36bf11466
Charles Evangelista <kizaru03012002@gmail.com>
2026-05-06 22:49:50 +0800

    backend

---
+++ app/api/auth/signin-with-oauth/route.ts
@@ -0,0 +1,10 @@
+import connectToDatabase from "@/lib/mongoose";
+import mongoose from "mongoose";
+
+export async function POST(request: Request) {
+  const { provider, providerAccountId, user } = await request.json();
+
+  await connectToDatabase();
+
+  const session = await mongoose.startSession();
+}
